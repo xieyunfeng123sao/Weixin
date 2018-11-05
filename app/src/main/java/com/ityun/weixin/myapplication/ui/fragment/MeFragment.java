a35716b378fa3835992894d4b03818f380d81831
xieyunfeng123 <337675907@qq.com>
2018-11-05 18:02:50 +0800

    1111

--- app/src/main/java/com/ityun/weixin/myapplication/ui/fragment/MeFragment.java
+++ app/src/main/java/com/ityun/weixin/myapplication/ui/fragment/MeFragment.java
@@ -7,7 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ityun.weixin.myapplication.R;
+
 import org.greenrobot.eventbus.Subscribe;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2018/2/12 0012.
@@ -17,7 +21,9 @@ public class MeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view=inflater.inflate(R.layout.fragment_me,container,false);
+        ButterKnife.bind(this,view);
+        return view;
     }
     @Subscribe
     public void  meEvent()
