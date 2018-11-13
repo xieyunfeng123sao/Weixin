e2ae3676e669ece3dd6492b2e5262126319ccdcf
xieyunfeng123 <337675907@qq.com>
2018-11-13 13:27:45 +0800

    添加我的界面和发现界面

--- app/src/main/java/com/ityun/weixin/myapplication/ui/fragment/FindFragment.java
+++ app/src/main/java/com/ityun/weixin/myapplication/ui/fragment/FindFragment.java
@@ -7,7 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ityun.weixin.myapplication.R;
+
 import org.greenrobot.eventbus.Subscribe;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2018/2/12 0012.
@@ -18,13 +22,14 @@ public class FindFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_find, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
 
     @Subscribe
-    public void  findEvent()
-    {
+    public void findEvent() {
 
     }
 }
